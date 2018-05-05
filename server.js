8788a795e5e9298d5b6c5da51eb027baf3dacc2c
Shreyas C R <coolshrey28@gmail.com>
2018-05-06 01:25:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articles={
-    'article-one':{
+
+var articleone={
         title:'article one',
         date:'18th sept',
         heading:'article-one',
@@ -17,71 +17,61 @@ var articles={
                     sd
         </p>
         '
-},
-    'article-two':{
-        title:'article two',
-        date:'19th sept',
-        heading:'article-two',
-        }
-}
+};
 function createTemplete(data){
     var title=data.title;
     var date=data.date;
     var content=data.content;
     var heading=data.heading;
     var htmltemplete=`
-        <html>
-        <head>
-            <title>
-                ${title}
-            </title>
-            <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-        
-        <body>
-            <div class="container">
-            <div>
-                <a href="/">Home</a>
-            </div>
-            <hr/>
-            <h3>
-                ${heading}
-            </h3>
-            <div>
-                ${date}
-            </div>
-            <div>
-                <p>
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            <p>
                 ${content}
-                </p>
-            </div>
-            </div>
-        </body>
-    </html>
-    `;
+            </p>
+        </div>
+        </div>
+    </body>
+    
+    
+</html>
+;
     return htmltemplete;
 }
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName',function(req,res){
-   var articleName=req.params.articleName;
-   res.send(createTemplete(articles[articleName));
+app.get('/articleone',function(req,res){
+  //var articleName=req.params.articleName;
+   res.send(createTemplete(articleone));
 });
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
-
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
-
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
