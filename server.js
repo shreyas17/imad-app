505413aec05ab332b2df6b3fd52e9339bd6cad5f
Shreyas C R <coolshrey28@gmail.com>
2018-05-06 01:03:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,20 +4,26 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-var articleone={
-    title:'article one',
-    date:'18th sept',
-    heading:'article-one',
-    content:`
-    <p>
-                khshyyvsi
-    </p>
-    <p>
-                sd
-    </p>
-    `
-};
+var articles={
+    'article-one':{
+        title:'article one',
+        date:'18th sept',
+        heading:'article-one',
+        content:`
+        <p>
+                    khshyyvsi
+        </p>
+        <p>
+                    sd
+        </p>
+        '
+},
+    'article-two':{
+        title:'article two',
+        date:'19th sept',
+        heading:'article-two',
+        }
+}
 function createTemplete(data){
     var title=data.title;
     var date=data.date;
@@ -59,8 +65,9 @@ function createTemplete(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-   res.send(createTemplete(articleone));
+app.get('/:articleName',function(req,res){
+   var articleName=req.params.articleName;
+   res.send(createTemplete(articles[articleName));
 });
 
 app.get('/ui/style.css', function (req, res) {
