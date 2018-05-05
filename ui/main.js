269b4643fe7879807c78856a235ba260938f584f
Shreyas C R <coolshrey28@gmail.com>
2018-05-06 02:24:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('hell');
 element.innerHTML='new valye';
+
+var img=document.getElementById('img');
+img.onClick=function(){
+  img.style.marginLeft='10px';  
+};
