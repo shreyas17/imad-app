0dae116523a6a3daf4a2f7e229815db80f010037
Shreyas C R <coolshrey28@gmail.com>
2018-05-05 23:46:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleone',function(req,res){
-   res.send("article one will be served"); 
+   res.sendFile(path.join(__dirname,'ui','articleone.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
