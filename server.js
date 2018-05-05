810987e10c3a5d77e325b8c5078dbab3f00feabe
Shreyas C R <coolshrey28@gmail.com>
2018-05-06 01:43:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ var htmltemplate=`
 <html>
  <head>
     <title>
-   ${title}
+      ${title}
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
  <link href="/ui/style.css" rel="stylesheet" />
