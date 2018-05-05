7af55cd914d7115325904f9f9087337600540eaa
Shreyas C R <coolshrey28@gmail.com>
2018-05-06 02:03:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/main.js', function (req, res) {
+app.get('/ui/main.js', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
