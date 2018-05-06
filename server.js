dc36dfa1bce0526f1d109cc865a5cdbd12c5a867
Shreyas C R <coolshrey28@gmail.com>
2018-05-06 23:40:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/', function (req, res) {
 });
 var names=[];
 app.get('/submit-name/:name',function(req,res){
-    var name=req.query.name;
+    var name=req.params.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
