75cb7cdf46480a260e2862adcca8d7fae3113c21
Shreyas C R <coolshrey28@gmail.com>
2018-05-06 22:38:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,13 @@
-console.log('Loaded!');
-//var element=document.getElementById('hell');
-//element.innerHTML='new valye';
-
-var imgElement = document.getElementById("img");
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+var button=document.getElementById('counter');
+button.onclick=function(){
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+      if(request.readyState===XMLHttpRequest.DONE){
+          if(request.status===200){
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
+      }
+  };
 };
