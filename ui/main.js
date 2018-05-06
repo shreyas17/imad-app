7dc21156aca5813b24e4d86724c5b84ba6f2d178
Shreyas C R <coolshrey28@gmail.com>
2018-05-06 23:51:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,14 +19,14 @@ button.onclick=function(){
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     var request=new XMLHttpRequest();
-  request.onreadystatechange=function(){
+    request.onreadystatechange=function(){
       if(request.readyState===XMLHttpRequest.DONE){
           if(request.status===200){
               var names=request.resonseText;
               names=JSON.parse(names);
               var list='';
               for(var i=0;i<names.length;i++){
-                  list<='<li>'+names[i]+'</li>';
+                  list+='<li>'+names[i]+'</li>';
               }
               var ul=document.getElementById('namelist');
               ul.innerHTML=list;
