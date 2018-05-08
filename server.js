462b380d54e192e331137fcda1e483db3ca0cdb4
Shreyas C R <coolshrey28@gmail.com>
2018-05-08 18:44:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,113 +1,114 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
-
-var config ={
-	user:'shreyaschitloor1996',
+var Pool=require('pg').Pool;
+var config={
+    user:'shreyaschitloor1996',
     database:'shreyaschitloor1996',
     host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
 };
-app.use(express.static("."));
-
-
 var app = express();
 app.use(morgan('combined'));
+app.use(express.static("."));
 
-
- var articles = {
-	 'article-one' : {
-		title	 :  'Article One | shreyas' ,
-		date	 : 'Feb 17, 2018' , 
-		heading	:  'Article One',
-		content	 : `<p>
-		                    This is the content for Article one This is the ontent for Article one This is the ontent for Article one This is the ontent for Article one
-		                    This is the content for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article one
-		                    
-		                </p>
-		            <p>
-		                    This is the content for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article one
-		            </p>`, 
-		},
-
-	 'article-two' : {
-			title	 :  'Article Two | shreyas' ,
-			date	 : 'Feb 18, 2018' , 
-			heading	:  'Article Two',
-			content	 : `<p>
-			                    This is the content for Second Article
-			            </p>`, 
-		},
-
-	 'article-three': {
-			title	 :  'Article Three | shreyas' ,
-			date	 : 'Feb 18, 2018' , 
-			heading	:  'Article Three',
-			content	 : `<p>
-			                    This is the content for Third Article
-			            </p>`, 
-		},
-
+var articles = {
+'article-one' : {
+    title: 'Article one|shreyas',
+    heading: 'article one',
+    date: 'feb 17,2018',
+    content:`
+    <p>
+        k
+    </p>
+    <p>
+        kk
+    </p>
+    <p>
+        kkk
+    </p>`
+    },
+'article-two' : {
+    title: 'Article two|shreyas',
+    heading: 'article two',
+    date: 'feb 17,2018',
+    content:`
+    <p>
+        k
+    </p>
+    <p>
+        kk
+    </p>
+    <p>
+        kkk
+    </p>`
+    
+},
+'article-three' : {
+    title: 'Article three|shreyas',
+    heading: 'article three',
+    date: 'feb 17,2018',
+    content:`
+    <p>
+        k
+    </p>
+    <p>
+        kk
+    </p>
+    <p>
+        kkk
+    </p>`
+    
+}
 };
 
-var pool = new Pool(config);
+function createTemplate (data) {
+var title = data.title;
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
 
-function createTemplate(data){
-	var title = data.title;
-	var date = data.date;
-	var heading = data.heading;
-	var content = data.content;
-	
-	var htmlTemplate = `
-	<html>
-	<head>
-	    <link href=ui/style.css rel="stylesheet"/>
-	        <title>
-	         ${title}
-	        </title>
-	    </head>
-	    <body>
-	        <div>
-	      <a href="/">Home</a>
-	    </div>
-	    <hr/>
-	    <h3>
-	        ${heading}
-	    </h3>
-	    <div>
-	        ${date}
-	    </div>
-	    <div>
-	    <p>
-	         ${content}
-	    </p>
-	    </div>
-	    </body>
-	</html>
-	`;
-	return htmlTemplate;
+
+var htmltemplate=`
+<html>
+ <head>
+    <title>
+      ${title}
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link href="/ui/style.css" rel="stylesheet" type="text/html" />
+ </head>    
+<body>
+     <div class="container">
+    <div>
+        <a href="/">home</a>
+    </div>
+<hr/>
+<h3>
+  ${heading}
+</h3>
+<div>
+  ${date}
+</div>
+<div>
+  ${content}
+</div>
+</div>
+ </body>
+</html>
+`;
+return htmltemplate;
 }
 
 var counter=0;
-app.get('/counter', function (req, res) {
-	counter+=1;
-  res.send(counter.toString());
+app.get('/counter',function(req,res){
+   counter=counter+1;
+   res.send(counter.toString());
 });
-
- var names= [];
- app.get('/submit-name', function (req, res) { //url: submit-name?query regarding name
-   var name = req.query.name; //also provided by xpress framework
-   names.push(name);
-   //JSON 
-   res.send(JSON.stringify(names));
-});
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
@@ -118,6 +119,30 @@ app.get('/test-db',function(req,res){
         }
     });
 });
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+    var name=req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
+app.get('/:articleName', function (req, res) {
+   var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/ui/main.js', function (req, res){
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
