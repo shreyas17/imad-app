22fcd5eb56e93bd79d15524b073f1df1e85cb3f8
Shreyas C R <coolshrey28@gmail.com>
2018-05-08 20:07:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/submit-name/:name',function(req,res){
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
    pool.query("SELECT * FROM article WHERE title='"+req.params.articleName,function(err,result){
        if(err){
            res.status(500).send(err.toString());
