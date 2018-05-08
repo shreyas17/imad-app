3e91cf89511910b992b6b2777bed8ba31fdbc9a1
Shreyas C R <coolshrey28@gmail.com>
2018-05-08 19:07:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,42 +63,40 @@ var articles = {
 }
 };
 
-function createTemplate (data) {
-var title = data.title;
-var date = data.date;
-var heading = data.heading;
-var content = data.content;
-
-
-var htmltemplate=`
-<html>
- <head>
-    <title>
-      ${title}
-    </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <link href="/ui/style.css" rel="stylesheet" type="text/html" />
- </head>    
-<body>
-     <div class="container">
-    <div>
-        <a href="/">home</a>
-    </div>
-<hr/>
-<h3>
-  ${heading}
-</h3>
-<div>
-  ${date}
-</div>
-<div>
-  ${content}
-</div>
-</div>
- </body>
-</html>
-`;
-return htmltemplate;
+function createTemplate(data){
+	var title = data.title;
+	var date = data.date;
+	var heading = data.heading;
+	var content = data.content;
+	
+	var htmlTemplate = `
+	<html>
+	<head>
+	    <link href=ui/style.css rel="stylesheet"/>
+	        <title>
+	         ${title}
+	        </title>
+	    </head>
+	    <body>
+	        <div>
+	      <a href="/">Home</a>
+	    </div>
+	    <hr/>
+	    <h3>
+	        ${heading}
+	    </h3>
+	    <div>
+	        ${date}
+	    </div>
+	    <div>
+	    <p>
+	         ${content}
+	    </p>
+	    </div>
+	    </body>
+	</html>
+	`;
+	return htmlTemplate;
 }
 
 var counter=0;
