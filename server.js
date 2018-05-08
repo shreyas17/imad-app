2444f79b96c56f12b47ca35ee7fcb6487c9c4156
Shreyas C R <coolshrey28@gmail.com>
2018-05-08 20:28:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get('/articles/:articleName', function (req, res) {
        }
        else{
            var articleData=result.rows[0];
-           res.send(createTemplete(articleData));
+           res.send(createTemplate(articleData));
        }
    });
 });
